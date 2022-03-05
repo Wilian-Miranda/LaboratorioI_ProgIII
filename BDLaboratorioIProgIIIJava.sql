f3d48d26752e22a9a3e91ade20e8a90546155e54
Wilian-Miranda <69541095+Wilian-Miranda@users.noreply.github.com>
2022-03-04 18:08:09 -0600

    Base de Datos
    Base de datos y tabla
    Proc almacenados

---
+++ BDLaboratorioIProgIIIJava.sql
@@ -0,0 +1,74 @@
+
+create database Lab;
+
+use lab;
+
+Create Table Contactos(
+	Id int not null auto_increment,
+    Nombre varchar(50),
+    Edad int,
+    Email varchar(50),
+    NumeroDeTelefono varchar(50),
+    primary key(Id)
+);
+
+--proc para mostrar contactos
+delimiter $$
+Create procedure sp_MostrarContactos()
+	
+begin
+	select * from Contactos;
+end$$
+
+call sp_MostrarContactos();
+
+--proc para insertar contactos
+delimiter $$
+create procedure sp_GuardarContacto(
+	Nombre varchar(50),
+    Edad int,
+    Email varchar(50),
+    NumeroDeTelefono varchar(50)
+)
+begin
+	insert into Contactos(Nombre,Edad,Email,NumeroDeTelefono) 
+				values(Nombre,Edad,Email,NumeroDeTelefono);
+end
+$$
+
+call sp_GuardarContacto('Jose wilian Leiva Miranda', 23, 'jwlm@gmail.com', '7589-3215');
+
+--proc para actualizar contacto
+delimiter $$
+create procedure sp_ActualizarContacto(
+	id int,
+	nombre varchar(50),
+    edad int,
+    email varchar(50),
+    numeroDeTelefono varchar(50)
+)
+begin
+	update Contactos set 
+    Nombre = nombre,
+    Edad= edad,
+    Email = email, 
+    NumeroDeTelefono = numeroDeTelefono
+    where Contactos.Id = id;
+end
+$$
+
+call sp_ActualizarContacto(2,'Ruben Hernández', 16, 'rb@gmail.com', '7777-3215');
+
+--proc eliminar contacto
+delimiter $$
+Create procedure sp_EliminarContacto(
+	id int
+)
+begin
+	delete from Contactos where Contactos.Id = id;
+end
+$$
+
+call sp_MostrarContactos();
+call sp_EliminarContacto(9);
+
